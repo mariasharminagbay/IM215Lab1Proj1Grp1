1dc5f83536273b24bc92a8585f22d0597b83bbd4
fpatel3 <forampatel3010@gmail.com>
2021-01-27 11:30:35 -0500

    made changes

--- Project 2/js/functions.js
+++ Project 2/js/functions.js
@@ -1,1 +1,7 @@
 /* add in your functions here */
+function outputCoutryBox(name, continent, cities,photoes) {
+    this.name = name;
+    this.iso = iso;
+    this.capital = capital;
+    this.population = population;
+}
