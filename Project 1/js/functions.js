424e025760f5bbd4b20ebe1898580cda744149af
fpatel3 <forampatel3010@gmail.com>
2021-01-27 09:51:01 -0500

    undid the change

--- Project 1/js/functions.js
+++ Project 1/js/functions.js
@@ -8,7 +8,7 @@ function outputCartRow(file, title, quantity, price, total) {
     // var file = filenames;
     console.log(file);
     document.write('<tr>')
-    document.write('<td><img src="images/' + file + '"></td>');
+    document.write('<td><img src="../images/' + file + '"></td>');
     document.write('<td>' + title + '</td>');
     document.write('<td>' + quantity + '</td>');
     document.write('<td>$' + price + '</td>');
