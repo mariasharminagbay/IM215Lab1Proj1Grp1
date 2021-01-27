dd19a770a1e8d7bb569673044e197ba8c0906abf
mariasharmagbay <mariasharmagbay@gmail.com>
2021-01-27 12:09:34 -0500

    Update on all the files to complete all the requirements
    Update on all the files to complete all the requirements

--- Project 1/js/chapter08-project01.js
+++ Project 1/js/chapter08-project01.js
@@ -1,35 +1,11 @@
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
 var subTotal = 0;
-console.log(filenames.length)
+
 for(i=0; i < filenames.length; i++){
    let amount = calculateTotal(quantities[i], prices[i]);
    let qty = quantities[i].toFixed(2);
    let prc = prices[i].toFixed(2);
    outputCartRow(filenames[i],titles[i],qty,prc,amount.toFixed(2));
    subTotal += parseFloat(amount.toFixed(2));
-   
 }
-
-// for SubTotal
-document.getElementById('subTotal').innerHTML = "$ " + subTotal.toFixed(2); 
-
-//compute for Tax:
-var tax = (subTotal * .10);
-document.getElementById('Tax').innerHTML = "$ " + tax.toFixed(2);
-
-//compute for shipping:
-var shipping = 0;
-if (subTotal > 1000){
-   shipping = 40;
-}
-else{shipping = 0;}
-
-document.getElementById('Shipping').innerHTML = "$ " + shipping.toFixed(2);
-
-//compute for GrandTotal
-var TotalAll = 0;
-TotalAll = subTotal - tax - shipping;
-
-document.getElementById('grndTotal').innerHTML = "$ " + TotalAll.toFixed(2);
-
--- Project 1/js/functions.js
+++ Project 1/js/functions.js
@@ -5,7 +5,6 @@ function calculateTotal(price, quantity) {
 }
 
 function outputCartRow(file, title, quantity, price, total) {
-    // var file = filenames;
     console.log(file);
     document.write('<tr>')
     document.write('<td><img src="../Project 1/images/' + file + '"></td>');
@@ -13,8 +12,28 @@ function outputCartRow(file, title, quantity, price, total) {
     document.write('<td>' + quantity + '</td>');
     document.write('<td>$' + price + '</td>');
     document.write('<td>$' + total + '</td>');
-    document.write('</tr>')
-    
+    document.write('</tr>')    
 }
 
-        
+//populate Subtotal, tax, shipping and grandtotal:
+//did not include code below at chapter08-project01.js since it reloads again the outputCartRow function on the page loads.
+// for SubTotal
+document.getElementById('subTotal').innerHTML = "$ " + subTotal.toFixed(2); 
+
+//compute for Tax:
+var tax = (subTotal * .10);
+document.getElementById('Tax').innerHTML = "$ " + tax.toFixed(2);
+
+//compute for shipping:
+var shipping = 0;
+if (subTotal > 1000){
+   shipping = 40;
+}
+else{shipping = 0;}
+document.getElementById('Shipping').innerHTML = "$ " + shipping.toFixed(2);
+
+//compute for GrandTotal
+var TotalAll = 0;
+TotalAll = subTotal - tax - shipping;
+document.getElementById('grndTotal').innerHTML = "$ " + TotalAll.toFixed(2);
+      
