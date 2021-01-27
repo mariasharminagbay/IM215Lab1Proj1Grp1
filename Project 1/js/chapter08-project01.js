458a167be96e4dfa83dfba4edc8d522c21b38be3
fpatel3 <forampatel3010@gmail.com>
2021-01-27 09:36:26 -0500

    made changes
    changed the path for images

--- Project 1/js/chapter08-project01.js
+++ Project 1/js/chapter08-project01.js
@@ -1,7 +1,7 @@
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
 var subTotal = 0;
-
+console.log(filenames.length)
 for(i=0; i < filenames.length; i++){
    let amount = calculateTotal(quantities[i], prices[i]);
    let qty = quantities[i].toFixed(2);
--- Project 1/js/functions.js
+++ Project 1/js/functions.js
@@ -5,8 +5,10 @@ function calculateTotal(price, quantity) {
 }
 
 function outputCartRow(file, title, quantity, price, total) {
+    // var file = filenames;
+    console.log(file);
     document.write('<tr>')
-    document.write('<td><img src="../images/' + file + '"></td>');
+    document.write('<td><img src="images/' + file + '"></td>');
     document.write('<td>' + title + '</td>');
     document.write('<td>' + quantity + '</td>');
     document.write('<td>$' + price + '</td>');
